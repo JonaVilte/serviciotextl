d1ddbc7f943f3370c44a68d06f2972d2d80141b4
abril <herbasabril4@gmail.com>
2025-10-06 16:04:13 -0300

    Se agregaron algunas cosas

--- __tests__/pedidos/visualizar_un_pedido_en_una_targeta.test.tsx
+++ __tests__/pedidos/visualizar_un_pedido_en_una_targeta.test.tsx
@@ -4,7 +4,14 @@ import TarjetaParaVisualizarUnPedido from '@/src/pedidos';
 
 describe('Registrar pedido - Visualización de Historial - visualizacion de un pedido', () => {
   test('Que tenga el nombre de la persona que encargo el pedido, el estado, precio y fecha de emicion', () => {
-    const { getByText } = render(<TarjetaParaVisualizarUnPedido />);
+    const { getByText } = render(
+      <TarjetaParaVisualizarUnPedido
+        nombre_del_encargado={'Mario Bargas'}
+        fecha_de_emision={new Date(1999, 8, 10)}
+        estado_del_pedio={'completado'}
+        precio={2700}
+      />
+    );
 
     expect(getByText('Nombre del Encargada del Pedido: Mario Bargas')).toBeVisible();
     expect(getByText('Fecha de Emicion: 10/09/1999')).toBeVisible();
